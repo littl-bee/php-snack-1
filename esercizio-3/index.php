82376b37b0d7e43391ff7922ef480954375f07a7
littl-bee <isabellacruz1610@gmail.com>
2025-02-17 12:16:21 +0100

    esercizio 3

---
+++ esercizio-3/index.php
@@ -0,0 +1,24 @@
+<!-- 
+
+Operazioni con variabili
+- Crea due variabili numeriche, sommale e stampale.  
+- Crea altre due variabili e moltiplicale tra loro, 
+stampando il risultato. 
+
+-->
+
+<?php
+$num = 5;
+$num2 = 3;
+
+$somma = $num + $num2;
+
+echo "La somma di {$num} e {$num2} è {$somma}.<br>";
+
+$num3 = 4;
+$num4 = 2;
+
+$moltiplicazione = $num3 * $num4;
+
+echo " Il prodotto di {$num3} e {$num4} è {$moltiplicazione}";
+?>
