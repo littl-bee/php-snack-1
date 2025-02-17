7fac5f866cf9421697e2e278a12362447f27a606
littl-bee <isabellacruz1610@gmail.com>
2025-02-17 11:46:10 +0100

    aggiunto esercizio 2

---
+++ esercizio-2/index.php
@@ -0,0 +1,15 @@
+<!-- 
+
+Crea tre variabili (`$nome`, `$eta`, `$citta`) e 
+assegnagli dei valori.  
+
+
+-->
+
+<?php 
+    $nome = "Bee";
+    $eta = 23;
+    $citta = "VR";
+
+echo "Mi chiamo {$nome}, ho {$eta} anni e abito a {$citta}!";
+?>
